6ed89ff7b94969528435e52e337050363b40e98d
Michal Galka <michal.galka@collabora.com>
2021-06-10 18:24:28 +0200

    Move handle_errors() to utils.errors
    Move handle_errors() method to utils.errors package where it fits
    better.
    
    Signed-off-by: Michal Galka <michal.galka@collabora.com>

--- app/utils/callback/lava.py
+++ app/utils/callback/lava.py
@@ -676,7 +676,7 @@ def add_tests(job_data, job_meta, lab_name, db_options,
         ret_code = 401
         msg = "Invalid test data from LAVA callback"
         utils.errors.add_error(errors, ret_code, msg)
-        handle_errors(ex, msg, errors)
+        utils.errors.handle_errors(ex, msg, errors)
 
     if callback is None:
         return None
@@ -687,7 +687,7 @@ def add_tests(job_data, job_meta, lab_name, db_options,
         ret_code = 500
         msg = "Internal error"
         utils.errors.add_error(errors, ret_code, msg)
-        handle_errors(ex, msg, errors)
+        utils.errors.handle_errors(ex, msg, errors)
 
     test_results = LavaResults(callback.results,
                                callback.meta,
@@ -698,19 +698,10 @@ def add_tests(job_data, job_meta, lab_name, db_options,
         ret_code, plan_doc_id, err = \
             utils.kci_test.import_and_save_kci_tests(plan.data, db_options)
         utils.errors.update_errors(errors, err)
-        handle_errors(errors=errors)
+        utils.errors.handle_errors(errors=errors)
 
     if not plan_doc_id:
         utils.LOG.warn("No test results")
         return None
 
     return plan_doc_id
-
-
-def handle_errors(ex=None, msg=None, errors=None):
-    if ex is not None:
-        utils.LOG.exception(ex)
-    if msg is not None:
-        utils.LOG.error(msg)
-    if errors:
-        raise utils.errors.BackendError(errors)
